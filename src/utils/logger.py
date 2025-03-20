62179da95b44b572fd750d68d50fab708e0f367b
Alexandre Teixeira <alext.oliver24@gmail.com>
2025-03-20 15:13:47 -0300

    feat: Add logger configuration

---
+++ src/utils/logger.py
@@ -0,0 +1,61 @@
+import logging
+
+
+def configure_logger(file_name: str = "app.log", level: int = logging.INFO) -> logging.Logger:
+    """
+    Configure logger and returns the logger.
+    
+    Parameters:
+        file_name (str): Name of the log file.
+        level (int): Logging level.
+
+    Returns:
+        logging.Logger: Logger object.
+    """
+    logging.basicConfig(
+        filename=file_name,
+        level=level,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+    logger = logging.getLogger(__name__)
+    return logger
+
+def get_logger(file_name: str = "app.log", level: int = logging.DEBUG) -> logging.Logger:
+    """
+    Get a logger with the specified name and level.
+
+    Parameters:
+        file_name - str: Name of the log file
+        level - int: Logging level
+
+    Returns:
+        logging.Logger: Logger object
+    """
+    # -- Create a logger
+    logger = logging.getLogger("finance-analyzer")
+    logger.setLevel(level)
+
+    # -- Check if handlers are already added
+    if not logger.handlers:
+        # -- Create a formatter
+        formatter = logging.Formatter(
+            "{asctime} - {levelname}: {message}",
+            style="{",
+            datefmt="%d-%m-%Y %H:%M:%S"
+        )
+
+        # -- Create a console handler
+        ch = logging.StreamHandler()
+        ch.setLevel(level)
+        ch.setFormatter(formatter)
+
+        # -- Create a file handler
+        fh = logging.FileHandler(file_name)
+        fh.setLevel(level)
+        fh.setFormatter(formatter)
+
+        # -- Add the handlers to the logger
+        logger.addHandler(ch)
+        logger.addHandler(fh)
+
+    return logger
